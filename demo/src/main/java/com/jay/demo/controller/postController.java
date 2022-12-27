d7cd4957e97ab46bb5895fb8be2bfe41d9c2dcab
chlwodud77 <uniform3000@naver.com>
2022-12-27 23:31:31 +0900

    temp: add posts api cors setting - cors origins , allowheaders for temporary

--- demo/src/main/java/com/jay/demo/controller/postController.java
+++ demo/src/main/java/com/jay/demo/controller/postController.java
@@ -6,10 +6,7 @@ import com.jay.demo.service.impl.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class postController {
 
     @Autowired
